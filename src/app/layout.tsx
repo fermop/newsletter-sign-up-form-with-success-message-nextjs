0ee1d4d626177033205bd36f1f31dd8a74147ef3
Fernando Pérez Mojica <fernandoperezmojica@gmail.com>
2026-02-11 17:03:36 -0600

    feat(ui): setup global styles, typography, and layout structure

---
+++ src/app/layout.tsx
@@ -0,0 +1,29 @@
+import type { Metadata } from "next";
+import { Roboto } from "next/font/google";
+import "./globals.css";
+
+const roboto = Roboto({
+  variable: "--font-roboto",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Frontend Mentor | Newsletter sign-up form with success message with Next.js",
+  description: "Sign-up form to simulate a success message!",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${roboto.variable} font-roboto antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
