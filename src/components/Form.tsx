c546f3730f8c8baceaf99f5e04449bcbcd043463
Fernando Pérez Mojica <fernandoperezmojica@gmail.com>
2026-02-13 12:07:42 -0600

    perf: show respective bg-img according to screen size (no images hidden)

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,4 +1,4 @@
-import Image from 'next/image'
+import Image, { getImageProps } from 'next/image'
 import Button from '@/components/Button'
 
 interface FormProps {
@@ -15,32 +15,46 @@ const formListItems = [
 ]
 
 export default function Form({ email, error, handleChange, handleSubmit }: FormProps) {
+  const common = { alt: 'Sign up illustration' }
+
+  const {
+    props: { src: mobile, ...rest },
+  } = getImageProps({
+    ...common,
+    width: 375,
+    height: 250,
+    src: '/images/illustration-sign-up-mobile.svg',
+  })
+
+  const {
+    props: { srcSet: tablet },
+  } = getImageProps({
+    ...common,
+    width: 400,
+    height: 596,
+    src: '/images/illustration-sign-up-tablet.svg',
+  })
+
+  const {
+    props: { srcSet: desktop },
+  } = getImageProps({
+    ...common,
+    width: 400,
+    height: 596,
+    src: '/images/illustration-sign-up-desktop.svg',
+  })
   return (
     <div className='min-h-dvh w-screen lg:max-w-233 lg:min-h-fit lg:max-h-160 flex flex-col justify-between bg-white rounded-4xl lg:flex lg:flex-row-reverse lg:p-6 lg:pl-16 lg:gap-16 animate-fade-in-up'>
       <section>
-        <Image
-          src='/images/illustration-sign-up-mobile.svg'
-          alt=''
-          width={44}
-          height={44}
-          className="w-full h-[40%] md:hidden"
-        />
-
-        <Image
-          src='/images/illustration-sign-up-tablet.svg'
-          alt=''
-          width={44}
-          height={44}
-          className="w-full h-[40%] hidden md:block lg:hidden"
-        />
-
-        <Image
-          src='/images/illustration-sign-up-desktop.svg'
-          alt=''
-          width={44}
-          height={44}
-          className="w-full h-full hidden lg:block"
-        />
+        <picture>
+          <source media="(min-width: 768px)" srcSet={tablet} />
+          <source media="(min-width: 1024px)" srcSet={desktop} />
+          <img
+            {...rest}
+            src={mobile}
+            className="w-full h-[40%] lg:h-full object-cover rounded-t-4xl lg:rounded-none lg:rounded-l-2xl" 
+          />
+        </picture>
       </section>
 
       <section className='lg:max-w-94 flex flex-col gap-6 px-6 lg:px-0 py-10 lg:py-18 text-neutral-blue-700'>
